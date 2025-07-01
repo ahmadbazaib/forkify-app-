de3dfce74d6970c7d7f3248c444cef86fe27729d
Ahmad <ahmadbazaib7@gmail.com>
2025-07-01 18:18:28 +0530

    implemented search results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,20 +1,20 @@
 import * as model from './model.js';
 import recipeView from './view/recipeView.js';
+import searchView from './view/searchView.js';
+import resultsView from './view/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import { loadRecipe } from './model.js';
 
-const recipeContainer = document.querySelector('.recipe');
-
-// https://forkify-api.herokuapp.com/v2
+if (module.hot) {
+  module.hot.accept();
+}
 
 ///////////////////////////////////////
 
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
 
     if (!id) return;
     recipeView.renderSpinner();
@@ -29,8 +29,27 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    // 1. Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+    resultsView.renderSpinner();
+
+    // 2. Load search results
+    await model.loadSearchResults(query);
+
+    // 3. Render Results
+    resultsView.render(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+controlSearchResults();
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
 // window.addEventListener('hashchange', controlRecipes);
